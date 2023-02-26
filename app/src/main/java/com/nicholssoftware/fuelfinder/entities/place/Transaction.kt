2aae7052b8fe7e0e94f940924b53a060e5e88490
Jon Nichols <nichojo89@gmail.com>
2023-02-26 15:07:34 -0500

    Transaction model

--- app/src/main/java/com/nicholssoftware/fuelfinder/entities/place/Transaction.kt
+++ app/src/main/java/com/nicholssoftware/fuelfinder/entities/place/Transaction.kt
@@ -1,3 +1,12 @@
 package com.nicholssoftware.fuelfinder.entities.place
 
-data class Transaction(val id: Int, val amount: Double, val tax: Double, val name: String, val address: String)
+import android.os.Build
+import android.os.Build.VERSION_CODES.O
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+@RequiresApi(O)
+data class Transaction @RequiresApi(Build.VERSION_CODES.O) constructor(val id: Int, val amount: Double, val tax: Double, val name: String, val address: String, val date: LocalDateTime = LocalDateTime.now(
+    ZoneId.systemDefault()))
