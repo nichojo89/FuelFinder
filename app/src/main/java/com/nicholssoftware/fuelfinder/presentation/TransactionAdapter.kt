e9f9f2925375acc5b4f201a278ad33733c5088e8
Jon Nichols <nichojo89@gmail.com>
2023-02-26 15:07:10 -0500

    Added datetime and currency formatters

--- app/src/main/java/com/nicholssoftware/fuelfinder/presentation/TransactionAdapter.kt
+++ app/src/main/java/com/nicholssoftware/fuelfinder/presentation/TransactionAdapter.kt
@@ -1,24 +1,35 @@
 package com.nicholssoftware.fuelfinder.presentation
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.nicholssoftware.fuelfinder.R
 import com.nicholssoftware.fuelfinder.entities.place.Transaction
+import java.text.NumberFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatterBuilder
+import java.util.*
 
 class TransactionAdapter(private val transactions: List<Transaction>, private val onClickListener: AdapterView.OnItemClickListener) : RecyclerView.Adapter<TransactionAdapter.ViewHolder>() {
     class ViewHolder(transactionView: View): RecyclerView.ViewHolder(transactionView) {
-        val tvName: TextView = itemView.findViewById(R.id.tvTransactionName)
-        val tvAddress: TextView = itemView.findViewById(R.id.tvTransactionAddress)
         val tvAmount: TextView = itemView.findViewById(R.id.tvTransactionAmount)
-        val tvTax: TextView = itemView.findViewById(R.id.tvTransactionTax)
-        val tvTotal: TextView = itemView.findViewById(R.id.tvTransactionTotal)
+        val tvDate: TextView = itemView.findViewById(R.id.tvTransactionDate);
         val card: CardView = itemView.findViewById(R.id.cvTransaction);
     }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val _dateTimeFormatter = DateTimeFormatterBuilder()
+        .parseCaseInsensitive() // add pattern
+        .appendPattern("MM/DD, hh:mm a")
+        .toFormatter(Locale.ENGLISH)
+
+    private val _currencyFormatter = NumberFormat.getCurrencyInstance()
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
@@ -30,13 +41,12 @@ class TransactionAdapter(private val transactions: List<Transaction>, private va
 
     override fun getItemCount(): Int = transactions.size
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val transaction = transactions[position]
-        holder.tvAddress.text =transaction.address
-        holder.tvName.text = transaction.name;
-        holder.tvAmount.text = transaction.amount.toString()
-        holder.tvTax.text = transaction.tax.toString()
-        holder.tvTotal.text = (transaction.amount + transaction.tax).toString()
+        holder.tvAmount.text = _currencyFormatter.format(transaction.amount)
+        holder.tvDate.text  = transaction.date.format(_dateTimeFormatter)
+
         holder.card.setOnClickListener {
             onClickListener.onItemClick(null,null,position,-1)
         }
